b1d0cbdd232676045912dbda3da01ebcd09814a6
shubham-dreamspring <sjain@dreamspring.org>
2023-03-28 11:12:56 +0530

    Sync the changes in tests

--- spec/car.spec.js
+++ spec/car.spec.js
@@ -1,8 +1,8 @@
 const Car = require("../model/car");
+const ParkingLot = require("../model/parkingLot");
 const ORM = require("../utils/orm");
 
 describe("Tests for Car", () => {
-
   beforeAll(() => {
     this.registration_no = "UP23123456";
   });
@@ -14,13 +14,16 @@ describe("Tests for Car", () => {
   it("parking with invalid registration no", () => {
     expect(function () {
       let car = new Car("UPsdaksd93459cd48");
-      car.park();
+      const parkingLot = new ParkingLot();
+      parkingLot.park(car);
     }).toThrow(new Error("Invalid Registration no"));
   });
 
   it("park car", () => {
     let car = new Car(this.registration_no);
-    let car_details = car.park();
+    const parkingLot = new ParkingLot();
+    let car_details = parkingLot.park(car);
+
     expect(car_details.registration_no).toBe(this.registration_no);
     expect(car_details.slot).toBeDefined();
     expect(car_details.park_timestamp).toBeDefined();
@@ -31,20 +34,23 @@ describe("Tests for Car", () => {
       this.registration_no
     );
     expect(car_in_db.registration_no).toBeDefined();
-    car.unpark();
+    parkingLot.unpark(car);
   });
 
   it("unpark with not parked registration no", () => {
     expect(function () {
       let car = new Car("KA23123456");
-      car.unpark();
+      const parkingLot = new ParkingLot();
+      parkingLot.unpark(car);
     }).toThrow(new Error("Car Not found"));
   });
-  
+
   it("unpark car", () => {
     let car = new Car(this.registration_no);
-    car.park(this.registration_no);
-    car.unpark();
+    const parkingLot = new ParkingLot();
+    parkingLot.park(car);
+    parkingLot.unpark(car);
+
     const orm = new ORM();
     let car_in_db = orm.findById(
       "car",
@@ -53,5 +59,4 @@ describe("Tests for Car", () => {
     );
     expect(car_in_db).toBeUndefined();
   });
-  
 });
