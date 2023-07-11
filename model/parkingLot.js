4d52be22bed1b8070fed1e7109f7d088e61383ba
shubham-dreamspring <sjain@dreamspring.org>
2023-07-11 14:32:51 +0530

    Refactor the parkingLot model

--- model/parkingLot.js
+++ model/parkingLot.js
@@ -1,50 +1,43 @@
-const CustomORM = require("../utils/orm.js");
+const Slot = require("./slot.js");
+const Car = require("./car");
 
 class ParkingLot {
-  park(car) {
-    if (car.isValidRegistrationNumber()) {
-      try {
-        if (car.isParked()) {
-          throw new Error("Car already parked");
-        }
-        car.setSlot(ParkingLot.getEmptySlot());
-        car.addCar();
-      } catch (e) {
-        if (e.message === "No empty slot" || "Car already parked") {
-          throw e;
-        }
-        throw new Error("Something went wrong");
-      }
-    } else {
-      throw new Error("Invalid Registration no");
-    }
+  static park(car) {
+    let slot = Slot.getEmptySlot();
+    slot.vehicle_id = car.id;
+    slot.timestamp = Date.now();
+    slot.update();
 
-    return car.allAttributes();
+    return {
+      registration_no: car.registration_no,
+      park_timestamp: slot.timestamp,
+      slot_id: slot.id,
+    };
   }
 
-  unpark(car) {
-    if (car.isParked()) {
-      try {
-        const orm = new CustomORM();
-
-        car.deleteCar();
-        orm.pushData("emptyslots", car.slot);
-      } catch (e) {
-        console.log(e);
-        throw new Error("Something went wrong");
-      }
-    } else {
-      throw new Error("Car Not found");
-    }
+  static unpark(car) {
+    Slot.findAndDelete("vehicle_id", car.id);
+    Car.findAndDelete("id", car.id);
   }
 
-  static getEmptySlot() {
-    const orm = new CustomORM();
-    const slot = orm.deleteLastOne("emptyslots");
-    if (!slot) {
-      throw new Error("No empty slot");
-    }
-    return slot;
+  static parkedCars(sortProperty = null, limit = null) {
+    let slots = Slot.getFilledSlot(sortProperty, limit);
+    let result = slots.map((slot) => ({
+      slot_id: slot.id,
+      registration_no: slot.car().registration_no,
+    }));
+    return result;
+  }
+
+  static findParkedCar(registration_no) {
+    let car = Car.findById("registration_no", registration_no);
+    let slot = Slot.findById("vehicle_id", car.id);
+
+    return {
+      park_timestamp: slot.timestamp,
+      registration_no: car.registration_no,
+      slot_id: slot.id,
+    };
   }
 }
 
